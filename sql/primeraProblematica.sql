4d8285c7f50581ed23f48a19dbc76bf51774c6ad
alejosandrini <alejo.sandrini@gmail.com>
2022-07-29 12:11:47 -0300

    Ordeno codigo y borro comentarios

--- sql/primeraProblematica.sql
+++ sql/primeraProblematica.sql
@@ -1,16 +1,5 @@
--- TODO:
--- Listo 1. Crear tabla tipos cliente, tipos cuenta y marcas tarjera
--- Listo 2. Tarjeta tiene: Numero, CVV, fecha otorgamiento, fecha exporacion, tipo tarjeta
--- Listo 3. Relacionar tarjetas con marca de tarjera (Primary key en marca)
--- Listo 4. Relacionar tarjetas con cliente (Primary key en cliente)
--- Listo 5. Insertar 500 tarjetas de credito
--- Listo 6. Crear tabla direcciones, relacionarla con cliente, empleado, sucursal
---  7. Insertar 500 direcciones (cliente y empleado pueden tener muchas direcciones, sucursal una)
--- Listo 8. Ampliar cuenta para que tenga tipo de cuenta
--- Listo 9. Asignar tipos de cuenta
--- Listo 10. Cambiar formato fecha (employee_hire_date en tabla empleado) a YYYY-MM-DD
-
 --  1.
+-- Creacion tabla de tipos cliente y su insert
 DROP TABLE IF EXISTS tipos_cliente;
 CREATE TABLE tipos_cliente (
     id_tipo_cliente integer PRIMARY KEY,
@@ -24,6 +13,7 @@ VALUES ("CLASSIC", 0, 0, 1),
     ("GOLD", 1, 1, 1),
     ("BLACK", 2, 5, 1);
 
+-- Creacion tabla de tipos cuenta y su insert
 DROP TABLE IF EXISTS tipos_cuenta;
 CREATE TABLE tipos_cuenta (
     id_tipo_cuenta integer PRIMARY KEY,
@@ -34,6 +24,7 @@ VALUES  ("Caja ahorro pesos"),
         ("Caja ahorro dolares"),
         ("Cuenta corriente");
 
+-- Creacion tabla de restricciones tipo cliente y su insert
 DROP TABLE IF EXISTS restricciones_tipo_cliente;
 CREATE TABLE restricciones_tipo_cliente (
     id_tipo_cliente integer NOT NULL UNIQUE,
@@ -51,6 +42,7 @@ VALUES  (1, 10000,150000,0,0.1,0),
         (2,20000,500000,0,0.05,10000),
         (3, 100000, NULL, 0, 0, 10000);
 
+-- Creacion tabla de marcas tarjeta y su insert
 DROP TABLE IF EXISTS marcas_tarjeta;
 CREATE TABLE marcas_tarjeta (
     id_marca_tarjeta integer PRIMARY KEY,
@@ -59,7 +51,8 @@ CREATE TABLE marcas_tarjeta (
 INSERT INTO marcas_tarjeta (marca)
 VALUES ('VISA'), ('MASTERCARD'), ('AMERICAN EXPRESS');
 
---  2. 3. y 4.
+--  3. y 4.
+-- Creacion tabla de tipo tarjeta y su insert
 DROP TABLE IF EXISTS tipo_tarjeta;
 CREATE TABLE tipo_tarjeta (
     id_tipo_tarjeta integer PRIMARY KEY,
@@ -68,41 +61,24 @@ CREATE TABLE tipo_tarjeta (
 INSERT INTO tipo_tarjeta (tipo)
 VALUES ('CREDITO'), ('DEBITO');
 
--- DROP TABLE IF EXISTS tarjetas;
--- CREATE TABLE tarjetas (
---     id_tarjeta integer PRIMARY KEY,
---     id_marca_tarjeta integer NOT NULL,
---     customer_id integer NOT NULL,
---     numero integer NOT NULL UNIQUE,
---     CVV integer NOT NULL, 
---     fecha_otorgamiento text NOT NULL, 
---     fecha_expiracion text NOT NULL, 
---     tipo_tarjeta text NOT NULL,
---     CONSTRAINT fk_tarjeta
---         FOREIGN KEY (id_marca_tarjeta)
---         REFERENCES marcas_tarjeta(id_marca_tarjeta), 
---         FOREIGN KEY (customer_id)
---         REFERENCES cliente(customer_id),
---         FOREIGN KEY (tipo_tarjeta)
---         REFERENCES tipo_tarjeta(id_tipo_tarjeta)
---     );
-
+-- Query para contar cuantas tarjetas hay de cada marca (VISA, MASTERCARD, AMERICAN EXPRESS)
 SELECT t.id_marca_tarjeta, marca, count(t.id_marca_tarjeta) 
 FROM tarjetas as t
 INNER JOIN marcas_tarjeta as mt
 WHERE t.id_marca_tarjeta = mt.id_marca_tarjeta
 GROUP by t.id_marca_tarjeta;
 
+-- Query para contar cuantas tarjetas hay de cada tipo (CREDITO, DEBITO)
 SELECT t.tipo_tarjeta, tipo, count(t.tipo_tarjeta) 
 FROM tarjetas as t
 INNER JOIN tipo_tarjeta
 WHERE t.tipo_tarjeta = id_tipo_tarjeta
 GROUP by t.tipo_tarjeta;
 
---  5. Insertar 500 tarjetas con www.generatedata.com
+--  2. y 5. Insertar 500 tarjetas con www.generatedata.com
 --  Realizado en data-tarjetas.sql
 
---  6. 
+--  6. Creacion de tabla de direcciones
 DROP TABLE IF EXISTS direcciones;
 CREATE TABLE direcciones (
     id_direccion integer PRIMARY KEY,
@@ -123,14 +99,16 @@ CREATE TABLE direcciones (
         REFERENCES sucursal(branch_id)
     );
 
---  7. 
+--  7. Insertar 500 direcciones con www.generatedata.com
+--  Realizado en data-direcciones.sql
 
 --  8. y 9.
--- ALTER TABLE cuenta
--- ADD COLUMN id_tipo_cuenta integer NOT NULL default(1);
--- ALTER TABLE cliente
--- ADD COLUMN id_tipo_cliente integer NOT NULL default(1);
+ALTER TABLE cuenta
+ADD COLUMN id_tipo_cuenta integer NOT NULL default(1);
+ALTER TABLE cliente
+ADD COLUMN id_tipo_cliente integer NOT NULL default(1);
 
+-- Dos updates a cuenta para que haya distintos tipos de cuentas
 UPDATE cuenta
 SET id_tipo_cuenta = 3
 WHERE balance < 0;
@@ -139,12 +117,14 @@ UPDATE cuenta
 SET id_tipo_cuenta = 2
 WHERE length(iban) > 22 AND balance > 0;
 
+-- Query para contar cuantos tipo de cuenta hay (Caja ahorro pesos, Caja ahorro dolares O Cuenta corriente)
 SELECT c.id_tipo_cuenta, tipo, count(c.id_tipo_cuenta)
 FROM cuenta as c
 INNER JOIN tipos_cuenta as tc
 WHERE c.id_tipo_cuenta = tc.id_tipo_cuenta
 GROUP BY (c.id_tipo_cuenta);
 
+-- Dos updates a cliente para que haya distintos tipos de cliente
 UPDATE cliente
 SET id_tipo_cliente = 3
 WHERE CAST(SUBSTR(dob,1,4) as INT) <= 1955;
@@ -153,55 +133,16 @@ UPDATE cliente
 SET id_tipo_cliente = 2
 WHERE CAST(SUBSTR(dob,1,4) as INT) < 1985 AND CAST(SUBSTR(dob,1,4) as INT) > 1955;
 
-SELECT count(customer_id) -- 135
-FROM cliente
-WHERE CAST(SUBSTR(dob,1,4) as INT) <= 1955;
-SELECT count(customer_id) -- 215
-FROM cliente
-WHERE CAST(SUBSTR(dob,1,4) as INT) < 1985 AND CAST(SUBSTR(dob,1,4) as INT) > 1955;
-SELECT count(customer_id) -- 150
-FROM cliente
-WHERE CAST(SUBSTR(dob,1,4) as INT) >= 1985;
+-- Query para contar cuantos clientes hay de cada tipo (CLASSIC, GOLD O BLACK)
+SELECT c.id_tipo_cliente, tipo, count(c.id_tipo_cliente)
+FROM cliente as c
+INNER JOIN tipos_cliente as tc
+WHERE c.id_tipo_cliente = tc.id_tipo_cliente
+GROUP BY (c.id_tipo_cliente);
+
 
 -- 10.
--- UPDATE empleado
--- SET employee_hire_date = SUBSTR(employee_hire_date,7,4) || '-' ||
---                          SUBSTR(employee_hire_date,4,2) || '-' || 
---                          SUBSTR(employee_hire_date,1,2);
-
-
--- entregar generador de tarjetas y direcciones a parte
--- 1. Marca de tarjeta, al final es credito o debito?
--- rta: visa o mastercar por ej, hacer tabla tipo de tarjeta
-
--- 2. Direcciones, como hacemos para que cliente y empleado tengan múltiples 
--- direcciones y sucursal una. 
--- (se me ocurre agregar dirección a sucursal y armar una tabla para el resto)
--- rta:
-
--- 3. En la segunda problemática, punto 1 que significa número de sucursal 
--- es branch_id?
--- rta: si
-
--- 4. En la segunda problemática, dice eliminar noel david, pero no existe, 
--- que hacemos?
--- rta: agregar un noel david
---
--- 5. Tercera problemática, “recordar que en las bases de datos la moneda se 
--- guarda como integer, en este caso con 2 centavos”, es decir si tenemos 
--- 30641711 sería 306.417,11
--- rta: los ultimos dos numeros son los decimales
---
--- 6. Tercera problemática, Seleccionar los préstamos que tengan fecha en abril, 
--- junio y agosto, ordenándolos por importe, primero ordenar por mes o no?
--- rta: todo mezclado, solo por importe
-
--- 7. Cuarta problemática, Obtener el promedio de créditos otorgado por sucursal,
---  créditos se refiere a préstamos de los clientes de cada sucursal?
--- rta: si
-
--- 8. En la cuarta problemática, “Mediante índices mejorar la performance la 
--- búsqueda de clientes por DNI”
--- rta: solo crear el indice por dni
--- create UNIQUE index idx_contacts_email
--- on cliente (customer_DNI);
+UPDATE empleado
+SET employee_hire_date = SUBSTR(employee_hire_date,7,4) || '-' ||
+                         SUBSTR(employee_hire_date,4,2) || '-' || 
+                         SUBSTR(employee_hire_date,1,2);
