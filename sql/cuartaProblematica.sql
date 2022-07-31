4ebbb7d82e827b948904e37273418d764ecc9137
alejosandrini <alejo.sandrini@gmail.com>
2022-07-31 19:52:10 -0300

    Resuelvo punto 7

--- sql/cuartaProblematica.sql
+++ sql/cuartaProblematica.sql
@@ -1,11 +1,5 @@
 -- TODO:
 -- 2. Revisar, esperar respuesta
--- 7. Crear tabla movimientos, campos: identificación del movimiento,
--- número de cuenta, monto, tipo de operación y hora
---  Mediante el uso de transacciones, hacer una transferencia de 1000$
--- desde la cuenta 200 a la cuenta 400
--- Registrar el movimiento en la tabla movimientos
--- En caso de no poder realizar la operación de forma completa, realizar un ROLLBACK
 
 -- 1. Listar cantidad de clientes por sucursal descendiente
 SELECT c.branch_id, s.branch_name as Sucursal,
@@ -85,4 +79,38 @@ WHERE account_id BETWEEN 10 AND 14;
 create UNIQUE index idx_contacts_email
 on cliente(customer_DNI);
 
--- 7. 
+-- 7. Crear tabla movimientos y realizar dos transacciones
+DROP TABLE IF EXISTS movimientos;
+CREATE TABLE movimientos(
+    id_movimiento integer PRIMARY KEY,
+    id_cuenta integer NOT NULL,
+    monto integer NOT NULL,
+    tipo_operacion text NOT NULL,
+    hora datetime NOT NULL,
+    CONSTRAINT fk_movimientos
+        FOREIGN KEY (id_cuenta)
+        REFERENCES cuenta(customer_id)
+);
+
+BEGIN TRANSACTION;
+    UPDATE cuenta
+    SET balance = balance - 1000
+    WHERE customer_id = 200;
+    UPDATE cuenta
+    SET balance = balance + 1000
+    WHERE customer_id = 400;
+    INSERT INTO movimientos (id_cuenta, monto, tipo_operacion, hora)
+    VALUES  (200, 1000, '-', time('now', 'localtime')),
+            (400, 1000, '+', time('now', 'localtime'));
+COMMIT;
+
+BEGIN TRANSACTION;
+    UPDATE cuenta
+    SET balance = balance - 1000
+    WHERE customer_id = 200;
+    UPDATE cuenta
+    SET balance = balance + 1000
+    WHERE customer_id = 400;
+    INSERT INTO movimientos (id_cuenta, monto, tipo_operacion, hora)
+    VALUES  (200, 1000, '-', time('now', 'localtime'));
+ROLLBACK;
