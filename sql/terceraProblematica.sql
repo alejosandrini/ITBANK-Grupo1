9bfd517766fa1ca52b39a88faab0914d531a015f
alejosandrini <alejo.sandrini@gmail.com>
2022-08-21 15:20:31 -0300

    TODO

---
+++ sql/terceraProblematica.sql
@@ -0,0 +1,18 @@
+-- TODO:
+-- 1. Seleccionar las cuentas con saldo negativo
+-- 2. Seleccionar el nombre, apellido y edad de los clientes que tengan en el
+--      apellido la letra Z
+-- 3. Seleccionar el nombre, apellido, edad y nombre de sucursal de las personas
+--      cuyo nombre sea “Brendan” y el resultado ordenarlo por nombre de sucursal
+-- 4. Seleccionar de la tabla de préstamos, los préstamos con un importe mayor
+--      a $80.000 y los préstamos prendarios utilizando la unión de tablas/consultas
+--      (recordar que en las bases de datos la moneda se guarda como integer, en este caso con 2 centavos)
+-- 5. Seleccionar los prestamos cuyo importe sea mayor que el importe medio de
+--      todos los prestamos
+-- 6. Contar la cantidad de clientes menores a 50 años
+-- 7. Seleccionar las primeras 5 cuentas con saldo mayor a 8.000$
+-- 8. Seleccionar los préstamos que tengan fecha en abril, junio y agosto,
+--      ordenándolos por importe
+-- 9. Obtener el importe total de los prestamos agrupados por tipo de préstamos.
+--      Por cada tipo de préstamo de la tabla préstamo, calcular la suma de sus
+--      importes. Renombrar la columna como loan_total_accu
