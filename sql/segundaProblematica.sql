4ca302fdb63e1ec73f855c7ac050c7ab20dca142
biancuscaa <castoldibianca03@gmail.com>
2022-08-22 17:59:10 -0300

    segunda problematica

--- sql/segundaProblematica.sql
+++ sql/segundaProblematica.sql
@@ -1,14 +1,48 @@
 -- TODO:
--- 1.  Crear una vista con las columnas id, numero sucursal, nombre, apellido, 
---     DNI y edad de la tabla cliente
---     o Mostrar las columnas de los clientes, ordenadas por el DNI de menor
---         a mayor y cuya edad sea superior a 40 años
---     o Mostrar todos los clientes que se llaman “Anne” o “Tyler” ordenados
---         por edad de menor a mayor
--- 2. Insertar 5 nuevos clientes (estan en la consigna) en la base de datos y 
---      verificar que se haya realizado con éxito la inserción
--- 3. Actualizar 5 clientes recientemente agregados en la base de datos dado que
--- hubo un error en el JSON que traía la información, la sucursal de todos es la 10
--- 4. Eliminar el registro correspondiente a “Noel David” realizando la selección
--- por el nombre y apellido
+
 -- 5. Consultar sobre cuál es el tipo de préstamo de mayor importe
+
+-- PUNTO 1 --
+drop VIEW clienteview;
+CREATE VIEW clienteview AS SELECT customer_id,branch_id ,customer_name, customer_surname, customer_DNI, CAST(strftime('%Y.%m%d', 'now') - strftime('%Y.%m%d', dob) AS INT) AS customer_edad
+FROM cliente;
+
+SELECT * 
+FROM clienteview
+WHERE customer_edad>40
+ORDER BY customer_DNI;
+
+SELECT * 
+FROM clienteview
+WHERE customer_name= "Anne" or customer_name= "Tyler"
+ORDER BY customer_edad;
+
+-- PUNTO 2 --
+
+INSERT INTO cliente (customer_name, customer_surname, customer_DNI, branch_id, dob)
+VALUES ("Lois", "Stout", 47730534, 80, 1984-07-07),
+       ("Hall", "Mcconnell", 52055484, 45, 1988-04-30),
+       ("Hilei", "Mclean", 43825213, 77, 1993-03-28),
+       ("Jin", "Cooley", 21207908, 98, 1959-08-24),
+       ("Gabriel", "Harmon", 57083950, 27, 1978-04-01);
+
+-- PUNTO 3 --
+
+UPDATE cliente
+SET branch_id = 10
+WHERE customer_id>500;
+
+-- PUNTO 4 --
+
+INSERT INTO cliente (customer_name, customer_surname, customer_DNI, branch_id, dob)
+VALUES ("Noel", "David", 00000000, 00, 1984-07-07);
+DELETE FROM cliente
+WHERE customer_name= "Noel" and customer_surname= "David";
+
+-- PUNTO 5 --
+
+SELECT loan_type, max(loan_total)
+FROM prestamo;
+
+
+
