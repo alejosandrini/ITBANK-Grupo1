05be7b25d7dabbeea052b6d5c0376eb7dbc40698
biancuscaa <castoldibianca03@gmail.com>
2022-08-22 15:54:22 -0300

    login
    login

--- homebanking/Login/views.py
+++ homebanking/Login/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.shortcuts import render, redirect
 from django.views import View
+from Clientes.models import Cliente
 
 
 class LoginView(View):
@@ -11,8 +12,21 @@ class LoginView(View):
             context = request.GET.dict()
             return render(request, 'Login/signup.html', context=context)
 
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password) 
+        if user is not None: 
+            login(request, user)
+            cliente= Cliente.objects.get(id_usuario=user.id)    
+            return render(request, 'Cuentas/bank.html', {"cliente":cliente})       
+        else:
+            return redirect("signup")
+
+
 
 class LogoutView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
         return redirect('index')
+
