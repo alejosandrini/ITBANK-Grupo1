59466f36dd602e468d60b211494a361516902538
Lautaro Joaquin Fonseca <lautarojoaquinfonseca@gmail.com>
2022-08-24 21:18:21 -0300

    Added Loan Functionality and Code Cleanup
    Finished adding the loan functionality and cleaned up the code

--- homebanking/Cuentas/views.py
+++ homebanking/Cuentas/views.py
@@ -3,9 +3,39 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 
+from Clientes.models import Cliente
+from Clientes.serializers import ClienteSerializer
+from Cuentas.models import Cuenta
+from Prestamos.models import Prestamo
+
 
 class HomeBankingView(View):
     @method_decorator(login_required)
     def get(self, request):
         context = request.GET.dict()
+
+        cliente = Cliente.objects.filter(usuario_id=request.user.id).first()
+        cliente_serializado = ClienteSerializer(cliente).data
+
+        cuentas = Cuenta.objects.filter(customer_id=cliente.customer_id)
+
+        balances = []
+        balance_total = 0
+
+        for index, cuenta in enumerate(cuentas, 1):
+            amount = cuenta.balance / 100
+            balances.append({'amount': amount, 'id': index})
+            balance_total += amount
+
+        prestamos = []
+
+        for index, prestamo in enumerate(Prestamo.objects.filter(customer_id=cliente.customer_id), 1):
+            prestamos.append(
+                {'type': prestamo.loan_type, 'amount': prestamo.loan_total, 'date': prestamo.loan_date,
+                 'id': index})
+
+        request.session['cliente'] = cliente_serializado
+        request.session['cuentas'] = {'balanceTotal': round(balance_total, 2), 'balances': balances}
+        request.session['prestamos'] = prestamos
+
         return render(request, "Cuentas/bank.html", context=context)
--- homebanking/Login/views.py
+++ homebanking/Login/views.py
@@ -2,11 +2,7 @@ from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
-from Clientes.models import Cliente
-from Cuentas.models import Cuenta
 from django.contrib import messages
-
-from Clientes.serializers import ClienteSerializer
 
 
 class LoginView(View):
@@ -28,25 +24,9 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
 
-                cliente = Cliente.objects.filter(usuario_id=user.id).first()
-                clienteSerializado = ClienteSerializer(cliente).data
-
-                cuentas = Cuenta.objects.filter(customer_id=cliente.customer_id)
-
-                balances = []
-                balanceTotal = 0
-
-                for index, cuenta in enumerate(cuentas, 1):
-                    amount = cuenta.balance / 100
-                    balances.append({'amount': amount, 'id':index})
-                    balanceTotal += amount
-
-                request.session['cliente'] = clienteSerializado
-                request.session['cuentas'] = {'balanceTotal':round(balanceTotal, 2),'balances':balances}
-
                 return redirect('bank')
             else:
-                messages.warning(request,"La combinación campo-contraseña no es correcta. Intente nuevamente")
+                messages.warning(request, "La combinación campo-contraseña no es correcta. Intente nuevamente")
                 return redirect("signup")
         elif request.POST['signup'] is not None:
             user = User.objects.create_user(
@@ -64,4 +44,5 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         messages.info(request, "La sesión ha sido cerrada correctamente.")
+
         return redirect('index')
